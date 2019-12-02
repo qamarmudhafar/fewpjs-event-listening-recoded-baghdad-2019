6b7b0be32dd5f872ed04fde0874659b2db24178e
qamar mudhafar <qamarmudhafar@yahoo.com>
2019-12-02 00:57:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
 }
+const input = document.querySelector('input');
+
+function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
+}
